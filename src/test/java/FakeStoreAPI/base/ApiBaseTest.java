380ca0d610ad257ea539309f052ccdd69d579206
alvaroalt <alvaro.lataza@pluto.tv>
2026-02-09 04:31:52 -0300

    add call request and responses to logs

--- src/test/java/FakeStoreAPI/base/ApiBaseTest.java
+++ src/test/java/FakeStoreAPI/base/ApiBaseTest.java
@@ -1,6 +1,8 @@
 package FakeStoreAPI.base;
 
 import io.restassured.RestAssured;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
 import org.testng.annotations.BeforeClass;
 
 public class ApiBaseTest {
@@ -8,5 +10,6 @@ public class ApiBaseTest {
     @BeforeClass
     public void setup() {
         RestAssured.baseURI = "https://fakestoreapi.com";
+        RestAssured.filters(new RequestLoggingFilter(), new ResponseLoggingFilter());
     }
 }
